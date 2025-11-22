f0d1c2ab8b5764fcf59ee12bafa4476429e26e9a
root <root@ip-172-31-13-138.ap-south-1.compute.internal>
2025-11-22 22:30:21 +0000

    changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from AWS Fargate on ECS!"
+    return "kirupha server!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
